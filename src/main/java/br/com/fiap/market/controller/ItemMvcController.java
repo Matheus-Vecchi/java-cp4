f82a974ab4fe5624b79a007c091986c8938080bc
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-08-28 11:36:05 -0300

    feat: Montei o CRUD MVC utilizando novo controller e adicionando htmls e agora vou seguir no security

---
+++ src/main/java/br/com/fiap/market/controller/ItemMvcController.java
@@ -0,0 +1,60 @@
+package br.com.fiap.market.controller;
+
+
+import br.com.fiap.market.dto.ItemRequestDTO;
+import br.com.fiap.market.dto.ItemResponseDTO;
+import br.com.fiap.market.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+
+
+@Controller
+@RequestMapping("/itens")
+@RequiredArgsConstructor
+public class ItemMvcController {
+
+    private final ItemService itemService;
+
+    @GetMapping
+    public String listarItens(Model model) {
+        List<ItemResponseDTO> itens = itemService.listarItens();
+        model.addAttribute("itens", itens);
+        return "itens/listar";
+    }
+
+    @GetMapping("/novo")
+    public String novoItem() {
+        return "itens/formulario";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String editarItem(@PathVariable Long id,
+                             Model model) {
+        ItemResponseDTO item = itemService.buscarItemPorId(id);
+        model.addAttribute("item", item);
+        return "itens/editar";
+    }
+
+    @PostMapping
+    public String cadastrarItem(ItemRequestDTO dto) {
+        itemService.cadastrarItem(dto);
+        return "redirect:/itens";
+    }
+
+    @PutMapping("/{id}/editar")
+    public String atualizarItem(@PathVariable Long id, ItemRequestDTO dto) {
+        itemService.atualizarItem(id, dto);
+        return "redirect:/itens";
+    }
+
+    @DeleteMapping("/{id}/excluir")
+    public String excluirItem(@PathVariable Long id) {
+        itemService.excluirItem(id);
+        return "redirect:/itens";
+    }
+}
